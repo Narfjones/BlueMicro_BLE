474cb6e07a0a8b97c9af50636554abc4d2b431fc
Narfjones <38131442+Narfjones@users.noreply.github.com>
2024-05-15 08:40:35 -0400

    fix keymap

--- firmware/keyboards/redox_enc/keymaps/default/keymap.cpp
+++ firmware/keyboards/redox_enc/keymaps/default/keymap.cpp
@@ -61,11 +61,11 @@ void encoder_callback(int step)
 
 std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
     KEYMAP2ARRAY(KEYMAP(
-        KC_GRV,   KC_1,    KC_2,     KC_3,    KC_4,    KC_5, KC_MUTE, \
-        KC_ESC,   KC_Q,    KC_W,     KC_E,    KC_R,    KC_T, KC_LBRC, \
-        KC_TAB,   KC_A,    KC_S,     KC_D,    KC_F,    KC_G, KC_COPY,\
-        KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,    KC_B, KC_PASTE,\
-        KC_LCTL   KC_LGUI,  KC_LALT, KC_LCTRL, LAYER_1, KC_BSPC, KC_DEL \
+        KC_GRV,   KC_1,    KC_2,     KC_3,    KC_4,    KC_5,     KC_MUTE, \
+        KC_ESC,   KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,     KC_LBRC, \
+        KC_TAB,   KC_A,    KC_S,     KC_D,    KC_F,    KC_G,     KC_COPY,\
+        KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,     KC_PASTE,\
+        KC_LCTL,   KC_LGUI, KC_LALT, KC_LCTRL, LAYER_1, KC_BSPC, KC_DEL \
     ));
 
  
